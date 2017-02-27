d1ca31adca849daf5f5030ad5ab5956e827e9545
Oleg Obleukhov <oleg.obleukhov@innogames.com>
2017-02-27 16:21:41 +0100

    Syntax fix according puppet-lint

--- manifests/init.pp
+++ manifests/init.pp
@@ -22,40 +22,40 @@ class sysfs (
     ensure => installed
   }
 
-  if ($osfamily == "RedHat") and ($operatingsystemmajrelease == "7")  {
+  if ($::osfamily == 'RedHat') and ($::operatingsystemmajrelease == '7')  {
     file { '/usr/local/bin/sysfs-reload' :
       source => 'puppet:///modules/sysfs/sysfs-reload',
       owner  => root,
       group  => root,
       mode   => '0700',
-      before => File["/etc/systemd/system/sysfsutils.service"]
+      before => File['/etc/systemd/system/sysfsutils.service']
     }
     file { '/etc/systemd/system/sysfsutils.service' :
       source => 'puppet:///modules/sysfs/sysfsutils.service',
       owner  => root,
       group  => root,
       mode   => '0700',
-      before => Service["sysfsutils"]
+      before => Service['sysfsutils'],
     }
     exec { 'sysfsutils_reload_rhel':
-      command => '/usr/bin/awk -F= \'/(\S+)\s*=(\S+)/{cmd=sprintf("/bin/echo %s > /sys/%s",$2, $1); system(cmd)}\' /etc/sysfs.conf',
+      command     => '/usr/bin/awk -F= \'/(\S+)\s*=(\S+)/{cmd=sprintf("/bin/echo %s > /sys/%s",$2, $1); system(cmd)}\' /etc/sysfs.conf',
       refreshonly => true,
-      subscribe => Concat['/etc/sysfs.conf'];
-    } 
+      subscribe   => Concat['/etc/sysfs.conf'],
+    }
   }
 
   service { 'sysfsutils':
     ensure    => running,
     enable    => true,
-    subscribe => Concat["/etc/sysfs.conf"]
+    subscribe => Concat['/etc/sysfs.conf'],
   }
 
-  concat { "/etc/sysfs.conf":
+  concat { '/etc/sysfs.conf':
     owner   => 'root',
     group   => 'root',
     mode    => '0644',
     force   => true,
-    require => Package["sysfsutils"];
+    require => Package['sysfsutils'],
   }
 
   if is_hash($settings) {
